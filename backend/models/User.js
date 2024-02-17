78220476218eeaf8924de10de6f1b2f9b1ca88a6
KPSaiRahul <sairahulkp3@Gmail.com>
2024-02-18 01:24:52 +0530

    created a collection in thunderclient

--- backend/models/User.js
+++ backend/models/User.js
@@ -20,6 +20,6 @@ const UserSchema = new Schema({
         default:Date.now()
     }, 
 });
-
-
-module.exports = mongoose.model('user', UserSchema);
+const User = mongoose.model('user', UserSchema);
+// User.createIndexes();    //CREATE INDEXES FOR EACH VALUE HENCE wherever unique:true, IT WILL CREATE UNIQUE INDEX
+module.exports = User;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,16 +1,45 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const { query,body, validationResult } = require('express-validator');
 
-//creating a user using POST "/api/auth/". doesn't require auth
-router.post('/', (req, res) =>{
-    {
-        name:"yesy"
+//creating a user using POST "/api/auth/createuser". No login required.  doesn't require auth
+router.post('/createuser',[
+    body('name','Enter a valid email').isLength({min:3}),    // VALIDATING USING EXPRESS-VALIDATOR
+    body('email', 'Enter a valid email').isEmail() ,
+    body('password', 'Password must be atleast 5 characters long').isLength({min:5})
+] ,async (req, res) =>{
+
+    //if there are errors, return bad req and the errors
+    const errors = validationResult(req);
+    
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors:errors.array()});
     }
-    console.log(req.body);
-    res.send("hello");
-    const user = User(req.body);  
-    user.save();    //saving user to mongodb
-    res.json(obj);
+    //Check whether the user email already exists
+    try{
+    let user =await User.findOne({email:req.body.email});
+    if(user){
+        return res.status(400).json({error:"Sorry a user with this email already exists"})
+    }
+    //Create a User
+    user = await User.create({
+        name :req.body.name,
+        email :req.body.email,
+        password:req.body.password
+    })
+    // .then(user=>res.json(user))
+    // .catch(err => {console.log(err)
+    res.json(user);
+
+    // const user = User(req.body);      ||  MANUAL METHOD NO VERIFICATION
+    // user.save();    //saving user to mongodb
+    // res.json(obj);
+    }
+    catch(error){
+        console.log(error.message);
+        res.status(500).send("Some error occurred: " + error.message);
+
+    }
 })
 module.exports = router;
