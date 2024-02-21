fa60d67bd1d6451a388f98f50ca0437685cf92a0
KPSaiRahul <sairahulkp3@Gmail.com>
2024-02-21 13:46:55 +0530

    added add a note

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import About from "./components/About";
 import NoteState from "./context/notes/NoteState";
+import Alert from "./components/Alert";
 function App() {
   return (
     <>
       <NoteState>
       <BrowserRouter>
         <NavBar />
+        <Alert message="react course"/>
         <div className="container">
         <Routes>
           <Route exact path="/" element={<Home />} />
